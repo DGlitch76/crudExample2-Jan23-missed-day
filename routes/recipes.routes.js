27b6ba8305d300e99b11a3416c9e5f3e979a9498
Doomfang3 <saubinmathieu@gmail.com>
2023-02-06 15:01:15 +0100

    Add update and delete routes/views

--- routes/recipes.routes.js
+++ routes/recipes.routes.js
@@ -3,7 +3,7 @@ const Recipe = require('../models/Recipe.model')
 const router = express.Router()
 
 /* GET all recipes */
-router.get('/', async (req, res, next) => {
+router.get('/', async (req, res) => {
   try {
     const allRecipes = await Recipe.find()
     console.log('All recipes :', allRecipes)
@@ -14,13 +14,38 @@ router.get('/', async (req, res, next) => {
 })
 
 router.get('/new', async (req, res, next) => {
-  res.render('recipes/new')
+  res.render('recipes/new', { update: false })
+})
+
+router.get('/:recipeId', async (req, res) => {
+  const recipeFound = await Recipe.findById(req.params.recipeId)
+  res.render('recipes/one', { recipeFound })
 })
 
 router.post('/new', async (req, res) => {
   const body = req.body
   console.log(body)
-  const newRecipe = await Recipe.create({ ...body, ingredients: body.ingredients.split(' ') })
+  await Recipe.create({ ...body, ingredients: body.ingredients.split(' ') })
+
+  res.redirect('/recipes')
+})
+
+router.get('/:recipeId/update', async (req, res) => {
+  const recipe = await Recipe.findById(req.params.recipeId)
+  res.render('recipes/new', { recipe, update: true })
+})
+
+router.post('/:recipeId/update', async (req, res) => {
+  await Recipe.findByIdAndUpdate(req.params.recipeId, {
+    ...req.body,
+    ingredients: req.body.ingredients.split(' '),
+  })
+
+  res.redirect(`/recipes/${req.params.recipeId}`)
+})
+
+router.post('/:recipeId/delete', async (req, res) => {
+  await Recipe.findByIdAndDelete(req.params.recipeId)
 
   res.redirect('/recipes')
 })
