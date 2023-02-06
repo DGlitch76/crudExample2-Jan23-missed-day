51970307aa8b7318d0a3284123f2a82c9df2181e
Doomfang3 <saubinmathieu@gmail.com>
2023-02-06 10:26:59 +0100

    Add model for the recipe

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,7 +1,23 @@
 const { Schema, model } = require('mongoose')
 
 const recipeSchema = new Schema(
-  {},
+  {
+    ingredients: {
+      type: [String],
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    cookingTime: {
+      type: Number,
+    },
+    difficulty: {
+      enum: ['easy', 'medium', 'hard'],
+    },
+  },
   {
     // this second object adds extra properties: `createdAt` and `updatedAt`
     timestamps: true,
